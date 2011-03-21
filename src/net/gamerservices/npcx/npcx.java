d44e38280e11b6313792e9e2dc7258f7a8ae6d3b
Michael Bolland <mixxit@soliniaonline.com>
2011-03-21 03:43:38 +0000

    speed change

--- src/net/gamerservices/npcx/npcx.java
+++ src/net/gamerservices/npcx/npcx.java
@@ -186,7 +186,7 @@ public class npcx extends JavaPlugin {
 	
 	public void think()
 	{
-		tick.schedule(new Tick(this), 1 * 400);
+		tick.schedule(new Tick(this), 1 * 700);
 		// check npc logic
 		for (BasicHumanNpc npc : npclist.values())
 		{
