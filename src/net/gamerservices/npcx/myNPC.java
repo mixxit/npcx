ab8134c259391d0577eaf807dbfb78d3b55f6046
Michael Bolland <mixxit@soliniaonline.com>
2011-03-21 03:20:13 +0000

    added check for trigger named default

--- src/net/gamerservices/npcx/myNPC.java
+++ src/net/gamerservices/npcx/myNPC.java
@@ -23,6 +23,7 @@ public class myNPC {
 	public void parseChat(myPlayer myplayer, String message)
 	{
 		int count = 0;
+		int size = 0;
 		//myplayer.player.sendMessage("Parsing:" + message + ":" + Integer.toString(this.triggerwords.size()));
 		String message2=message+" ";
 		for (String word : message2.split(" "))
@@ -37,15 +38,35 @@ public class myNPC {
 						
 							myplayer.player.sendMessage(npc.getName() + " says to you, '"+ tw.response +"'");
 							count++;
-						
+							return;
 	
 					}
+					size++;
 				}
 			}
 		}
-		if (count == 0)
+		if (count == 0 && size == 0)
 		{
 			myplayer.player.sendMessage(npc.getName() + " says to you, 'I'm sorry. I'm rather busy right now.'");
+		} else {
+				int count2 = 0;
+				for (myTriggerword tw : triggerwords.values())
+				{
+					//myplayer.player.sendMessage("Test:" + word + ":"+ tw.word);
+					if (tw.word.toLowerCase().contains("default") && size > 0 && count == 0)
+					{
+						myplayer.player.sendMessage(npc.getName() + " says to you, '" + tw.response + "'");
+						count2++;
+						return;
+					}
+					
+					
+				}
+				if (count2 == 0)
+				{
+					myplayer.player.sendMessage(npc.getName() + " says to you, 'I'm sorry. I'm rather busy right now.'");
+				}
+				
 		}
 	}
 }
