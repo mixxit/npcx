0bc2a24472bd62863e0a33e726e4664651b7543a
Nijiko Yonskai <me@nijikokun.com>
2011-03-26 09:54:38 -0400

    Formatting, removal of un-neccessary coding.

--- src/net/gamerservices/npcx/npcxEListener.java
+++ src/net/gamerservices/npcx/npcxEListener.java
@@ -1,238 +1,156 @@
 package net.gamerservices.npcx;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Logger;
-import org.bukkit.craftbukkit.CraftServer;
+
 import org.bukkit.event.entity.*;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Monster;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.HumanEntity;
-import org.bukkit.entity.Zombie;
 
 import redecouverte.npcspawner.BasicHumanNpc;
 import redecouverte.npcspawner.NpcEntityTargetEvent;
 import redecouverte.npcspawner.NpcEntityTargetEvent.NpcTargetReason;
-import redecouverte.npcspawner.NpcSpawner;
 
-public class npcxEListener extends EntityListener 
-{
-	
-	private final npcx parent;
-	
-	public npcxEListener(npcx parent) 
-	{
+public class npcxEListener extends EntityListener {
+
+    private final npcx parent;
+
+    public npcxEListener(npcx parent) {
         this.parent = parent;
     }
-	
-	@Override
-	public void onEntityDamage(EntityDamageEvent event) {
-		super.onEntityDamage(event);
-		
-		if (event instanceof EntityDamageByEntityEvent )
-		{
-			if (event.getEntity() instanceof Monster)
-			{
-				EntityDamageByEntityEvent edee1 = (EntityDamageByEntityEvent)event;
-				if (edee1.getDamager() instanceof HumanEntity) 
-				{
-					
-					//System.out.println("npcx : monster go ow");
-				}
-					
-				
-		    }
-		}
-		
-		if (event.getEntity() instanceof HumanEntity) 
-		{
-			BasicHumanNpc npc = parent.npclist.getBasicHumanNpc(event.getEntity());
-			if (event instanceof EntityDamageByEntityEvent)
-		    {
-				EntityDamageByEntityEvent edee = (EntityDamageByEntityEvent) event;
 
-				/*if (npc != null && npc.aggro != null && edee.getDamager() == npc.aggro) 
-				{
-					npc.follow = null;
-					npc.aggro = null;
-					//System.out.println("npcx : forgot about target");
-						 
-   			    }*/
-		        if (npc != null && edee.getDamager() instanceof LivingEntity) 
-		        {
-	
-		        	Entity p = edee.getDamager();
-		        	if (npc.parent != null)
-		        	{
-		        		for (myPlayer player : parent.players.values())
-		        		{
-		        			if (player.player == edee.getDamager())
-		        			{
-		        				if (npc.aggro == null)
-		        				{
-			        				npc.parent.onPlayerAggroChange(player);
-		        					
-		        				} else {
-		        					// else dont fire again
-		        				}
-		        				
-		        				
-		        			}
-		        		}
-		        	}
-		            npc.follow = (LivingEntity)p;
-		            npc.aggro = (LivingEntity)p;
-		            
-		            try
-		            {
-		            	npc.hp = npc.hp - 10;
-		            	if (npc.hp < 1)
-		            	{
-		            		
-		            		
-		            		parent.onNPCDeath(npc);
-		            		
-		            	}
-		            } 
-		            catch (Exception e)
-		            {
-		            	npc.follow = null;
-						npc.aggro = null;
-						//System.out.println("npcx : forgot about target");
-		            	// do not modify mobs health
-		            }
-		            
-		            event.setCancelled(true);
-	
-		        }
-		    }
-		}
-	}
-	@Override
-	public void onEntityDeath	(	EntityDeathEvent 	event	)
-	{
-		
-		
-		if (event.getEntity() instanceof Monster)
-		{
-			
-			//System.out.println("npcx : deregistered monster");
-			this.parent.monsters.remove((Monster)event.getEntity());	
+    @Override
+    public void onEntityDamage(EntityDamageEvent event) {
+        super.onEntityDamage(event);
 
-		}
-	}
-	@Override
-	public void onCreatureSpawn	( CreatureSpawnEvent event)	
-	{
-		
-		if (event.getEntity() instanceof Monster)
-		{
-			//System.out.println("npcx : registered monster");
-			this.parent.monsters.add((Monster)event.getEntity());
-		}
-	}
+        if (event instanceof EntityDamageByEntityEvent) {
+            if (event.getEntity() instanceof Monster) {
+                EntityDamageByEntityEvent edee1 = (EntityDamageByEntityEvent) event;
+                if (edee1.getDamager() instanceof HumanEntity) {
+                    //System.out.println("npcx : monster go ow");
+                }
+            }
+        }
 
-	
-	
-	
-	@Override
-	
-    public void onEntityTarget(EntityTargetEvent event) {
-
-		
-		//System.out.println("npcx : target onentityevent");
-		
-        if (event instanceof NpcEntityTargetEvent) {
-            NpcEntityTargetEvent nevent = (NpcEntityTargetEvent)event;
-
-/*            for (myNPC n : parent.npcs.values())
-            {
-            }*/
-            
+        if (event.getEntity() instanceof HumanEntity) {
             BasicHumanNpc npc = parent.npclist.getBasicHumanNpc(event.getEntity());
-    		//System.out.println("npcx : this is an instance of npcentitytargetevent");
-            
-            
-            // Targets player
-    		if (npc == null)
-    		{
-    			
-    			//System.out.println("npcx : npc is null");
-    			/*for (myNPC n : parent.npcs.values())
+            if (event instanceof EntityDamageByEntityEvent) {
+                EntityDamageByEntityEvent edee = (EntityDamageByEntityEvent) event;
+
+                /*if (npc != null && npc.aggro != null && edee.getDamager() == npc.aggro)
                 {
-    				if (nevent.getTarget() == n.npc)
-    				{
-    					//System.out.println("npcx : npc found!");
-    					npc = n.npc;
-    				}
+                npc.follow = null;
+                npc.aggro = null;
+                //System.out.println("npcx : forgot about target");
+
                 }*/
-    			event.setCancelled(true);
-    		}
-    		
-            if (npc != null && event.getTarget() instanceof Player) 
-            {
-        		//System.out.println("npcx : npc is not null and target is a player");
+                if (npc != null && edee.getDamager() instanceof LivingEntity) {
+                    Entity p = edee.getDamager();
+                    if (npc.parent != null) {
+                        for (myPlayer player : parent.players.values()) {
+                            if (player.player == edee.getDamager()) {
+                                if (npc.attacking == null) {
+                                    npc.parent.onPlayerAggroChange(player);
+                                } else {
+                                    // else dont fire again
+                                }
+                            }
+                        }
+                    }
 
-            	if (nevent.getNpcReason() == NpcTargetReason.CLOSEST_PLAYER) {
-            		//System.out.println("npcx : reason of event was closest player");
+                    npc.following = (LivingEntity) p;
+                    npc.attacking = (LivingEntity) p;
 
-            		//Player p = (Player) event.getTarget();
+                    try {
+                        npc.health = npc.health - 10;
+                        if (npc.health < 1) {
+                            parent.onNPCDeath(npc);
+                        }
+                    } catch (Exception e) {
+                        npc.following = null;
+                        npc.attacking = null;
+                        //System.out.println("npcx : forgot about target");
+                        // do not modify mobs health
+                    }
+
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onEntityDeath(EntityDeathEvent event) {
+        if (event.getEntity() instanceof Monster) {
+            //System.out.println("npcx : deregistered monster");
+            this.parent.monsters.remove((Monster) event.getEntity());
+        }
+    }
+
+    @Override
+    public void onCreatureSpawn(CreatureSpawnEvent event) {
+        if (event.getEntity() instanceof Monster) {
+            //System.out.println("npcx : registered monster");
+            this.parent.monsters.add((Monster) event.getEntity());
+        }
+    }
+
+    @Override
+    public void onEntityTarget(EntityTargetEvent event) {
+        //System.out.println("npcx : target onentityevent");
+        if (event instanceof NpcEntityTargetEvent) {
+            NpcEntityTargetEvent nevent = (NpcEntityTargetEvent) event;
+            BasicHumanNpc npc = parent.npclist.getBasicHumanNpc(event.getEntity());
+            //System.out.println("npcx : this is an instance of npcentitytargetevent");
+
+            // Targets player
+            if (npc == null) {
+                event.setCancelled(true);
+            }
+
+            if (npc != null && event.getTarget() instanceof Player) {
+                //System.out.println("npcx : npc is not null and target is a player");
+                if (nevent.getNpcReason() == NpcTargetReason.CLOSEST_PLAYER) {
+                    //System.out.println("npcx : reason of event was closest player");
+                    //Player p = (Player) event.getTarget();
                     // player is near the npc
                     // do something here
-                	 /*
-                     if (npc != null) {
-                         npc.moveTo(event.getTarget().getLocation().getX(), event.getTarget().getLocation().getY(), event.getTarget().getLocation().getZ(), event.getTarget().getLocation().getYaw(), event.getTarget().getLocation().getPitch());
-                         
-                     }
+                     /*
+                        if (npc != null) {
+                            npc.moveTo(event.getTarget().getLocation().getX(), event.getTarget().getLocation().getY(), event.getTarget().getLocation().getZ(), event.getTarget().getLocation().getYaw(), event.getTarget().getLocation().getPitch());
+                        }
                      */
                     event.setCancelled(true);
-
                 } else if (nevent.getNpcReason() == NpcTargetReason.NPC_RIGHTCLICKED) {
-            		//System.out.println("npcx : reason of event was rightclicked");
+                    //System.out.println("npcx : reason of event was rightclicked");
 
-                	Player p = (Player) event.getTarget();
+                    Player p = (Player) event.getTarget();
 
-                    /*
-                    p.sendMessage("* You say, 'Hail, " + npc.getName() + "!'");
-                    p.sendMessage("* " + npc.getName() + " says, 'Hello " + p.getName() + "!");
-                    */
-                    
-                    for (myPlayer player : parent.players.values()){
-                    	
-                    	
-            			if (player.player == p)
-            			{
-            				if (player.target != null)
-            				{
+                    for (myPlayer player : parent.players.values()) {
+                        if (player.player == p) {
+                            if (player.target != null) {
                                 p.sendMessage("* Target cleared!");
                                 player.target = null;
-            					
-            				} else {
-            					player.target = npc;
+                            } else {
+                                player.target = npc;
                                 p.sendMessage("* Active chat target set as: " + npc.getName());
-            				}
-            				
-            			} else {
-            				if (player.name == p.getName())
-            				{
-            					p.sendMessage("Your name is right but your player is wrong");
-            				}
-            				
-            			}
-            		}
-                    
-                    
+                            }
+
+                        } else {
+                            if (player.name == p.getName()) {
+                                p.sendMessage("Your name is right but your player is wrong");
+                            }
+                        }
+                    }
+
                     event.setCancelled(true);
-                    
                 } else if (nevent.getNpcReason() == NpcTargetReason.NPC_BOUNCED) {
                     //Player p = (Player) event.getTarget();
                     // do something here
                     //p.sendMessage("<" + npc.getName() + "> Stop bouncing on me!");
                     event.setCancelled(true);
                 }
-            } 
+            }
         }
 
     }
--- src/net/gamerservices/npcx/npcxPListener.java
+++ src/net/gamerservices/npcx/npcxPListener.java
@@ -1,106 +1,74 @@
 package net.gamerservices.npcx;
-import java.util.logging.Logger;
-import org.bukkit.craftbukkit.CraftServer;
-import org.bukkit.event.entity.*;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerListener;
-import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.HumanEntity;
-import redecouverte.npcspawner.BasicHumanNpc;
-import redecouverte.npcspawner.NpcEntityTargetEvent;
-import redecouverte.npcspawner.NpcEntityTargetEvent.NpcTargetReason;
-import redecouverte.npcspawner.NpcSpawner;
 
 public class npcxPListener extends PlayerListener {
-	
-	private final npcx parent;
-	
-	public npcxPListener(npcx parent) {
+    private final npcx parent;
+
+    public npcxPListener(npcx parent) {
         this.parent = parent;
     }
-	public void onPlayerJoin(PlayerEvent event) 
-    {
-		myPlayer player = new myPlayer(event.getPlayer(),event.getPlayer().getName());
-		//System.out.println("npcx : added player ("+ event.getPlayer().getName()+")");
-		parent.players.put(player.player.getName(), player);
-		
-    }
-	
-	
-	
-	public void onPlayerChat(PlayerChatEvent event)
-    {
-		for (myPlayer player : parent.players.values()){
-			if (player.player == event.getPlayer())
-			{
-				if (player.target != null)
-				{
-					
-					//System.out.println("npcx : player chat event ("+ player.player.getName()+")");
-					player.player.sendMessage("You say to " + player.target.getName() +", '" + event.getMessage() + "'");
 
-					if (player.target.parent != null)
-					{
-						// this is not a temporary spawn
-						
-						// does it have a category set?
-						if (player.target.parent.category != null)
-						{
-							if (player.target.parent.category.matches("shop"))
-							{
-								// shop
-								player.target.parent.parseShop(player, event.getMessage());
-								
-							} else {
-								// normal chat event / unknown category
-								player.target.parent.parseChat(player, event.getMessage());
-							}
-						} else {
-							// normal chat event
-							player.target.parent.parseChat(player, event.getMessage());
-						}
-					} else {
-						player.player.sendMessage("You cannot talk to temporary spawns");
-					}
-					event.setCancelled(true);
-				}
-			}
-		}
+    public void onPlayerJoin(PlayerEvent event) {
+        myPlayer player = new myPlayer(event.getPlayer(), event.getPlayer().getName());
+        //System.out.println("npcx : added player ("+ event.getPlayer().getName()+")");
+        parent.players.put(player.player.getName(), player);
     }
-	
-	
-	public void onPlayerRespawn(PlayerRespawnEvent event)
-	{
-		for (myPlayer player : this.parent.players.values())
-		{
-				// deal with player death changes
-				if (player.player == event.getPlayer())
-				{
-					//System.out.println("npcx : player about to respawn, assigning them to the dead list");
-					player.dead = true;
-					
-				}
-			
-		}
-		
-	}
-	
-	public void onPlayerQuit(PlayerEvent event)
-    {
-		
-		for (myPlayer player : parent.players.values()){
-			if (player.player == event.getPlayer())
-			{
-				player.dead = true;
-				//System.out.println("npcx : removed player ("+ player.player.getName()+")");
-				parent.players.remove(player);
-			}
-		}
-		
-    
+
+    public void onPlayerChat(PlayerChatEvent event) {
+        for (myPlayer player : parent.players.values()) {
+            if (player.player == event.getPlayer()) {
+                if (player.target != null) {
+
+                    //System.out.println("npcx : player chat event ("+ player.player.getName()+")");
+                    player.player.sendMessage("You say to " + player.target.getName() + ", '" + event.getMessage() + "'");
+
+                    if (player.target.parent != null) {
+                        // this is not a temporary spawn
+
+                        // does it have a category set?
+                        if (player.target.parent.category != null) {
+                            if (player.target.parent.category.matches("shop")) {
+                                // shop
+                                player.target.parent.parseShop(player, event.getMessage());
+
+                            } else {
+                                // normal chat event / unknown category
+                                player.target.parent.parseChat(player, event.getMessage());
+                            }
+                        } else {
+                            // normal chat event
+                            player.target.parent.parseChat(player, event.getMessage());
+                        }
+                    } else {
+                        player.player.sendMessage("You cannot talk to temporary spawns");
+                    }
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        for (myPlayer player : this.parent.players.values()) {
+            // deal with player death changes
+            if (player.player == event.getPlayer()) {
+                //System.out.println("npcx : player about to respawn, assigning them to the dead list");
+                player.dead = true;
+            }
+        }
+    }
+
+    public void onPlayerQuit(PlayerEvent event) {
+        for (myPlayer player : parent.players.values()) {
+            if (player.player == event.getPlayer()) {
+                player.dead = true;
+                //System.out.println("npcx : removed player ("+ player.player.getName()+")");
+                parent.players.remove(player);
+            }
+        }
     }
 }
