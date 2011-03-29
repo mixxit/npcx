94eee07baaf4b4b1bef4ee579754528b8de99b27
Michael Bolland <mixxit@soliniaonline.com>
2011-03-29 14:58:21 +0100

    0.231 - working on invis npcs

---
+++ src/net/gamerservices/npcx/npcxWListener.java
@@ -0,0 +1,50 @@
+package net.gamerservices.npcx;
+
+import java.util.HashMap;
+
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.event.world.WorldListener;
+
+// Citizens Mod: http://forums.bukkit.org/threads/7173/
+
+public class npcxWListener extends WorldListener {
+	private npcx plugin;
+
+	public npcxWListener(npcx plugin) {
+		this.plugin = plugin;
+	}
+
+	@Override
+	public void onChunkUnloaded(ChunkUnloadEvent e) {
+		try
+		{
+			for (myNPC npc : plugin.universe.npcs.values())			
+			{
+				if (e.getChunk().getWorld()
+						.getChunkAt(npc.npc.getBukkitEntity().getLocation())
+						.equals(e.getChunk())) {
+					npc.npc.forceMove(npc.npc.getBukkitEntity().getLocation());
+				}
+			}
+		} catch (Exception e2) {
+			
+		}
+	}
+
+	public void onChunkLoaded(ChunkLoadEvent e) {
+		try
+		{
+			for (myNPC npc : plugin.universe.npcs.values())
+			{
+				if (e.getChunk().getWorld()
+						.getChunkAt(npc.npc.getBukkitEntity().getLocation())
+						.equals(e.getChunk())) {
+					npc.npc.forceMove(npc.npc.getBukkitEntity().getLocation());
+				}
+			}
+		} catch (Exception e2) {
+			
+		}
+	}
+}
